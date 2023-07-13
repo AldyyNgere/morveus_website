31f0ffa7a527f0270d5d8723da2325b5293aecdc
aldy ngere <aldyngere20@gmail.com>
2023-07-13 02:44:53 -0700

    menyelesaikan design header dan dark mode

---
+++ script.js
@@ -0,0 +1,50 @@
+// Navbar Mobile Button Function
+function navbarbtn() {
+  // get Element
+  const navbar_menu = document.querySelector(".navbar-menu");
+  const menu_btn = document.getElementById("menu-btn");
+  const close_btn = document.getElementById("close-btn");
+
+  //   when menu button clicked
+  menu_btn.onclick = () => {
+    menu_btn.classList.add("nav-active");
+    close_btn.classList.add("nav-active");
+    navbar_menu.classList.add("nav-active");
+  };
+
+  //   when close button clicked
+  close_btn.onclick = () => {
+    menu_btn.classList.remove("nav-active");
+    close_btn.classList.remove("nav-active");
+    navbar_menu.classList.remove("nav-active");
+  };
+}
+
+// Dark Mode Button Function
+function darkmode() {
+  // get all Element
+  const toggle_body = document.querySelector(".toggle-body");
+  const toggle = document.querySelector(".toggle");
+  const navbar = document.querySelector(".navbar");
+  const navbar_menu = document.querySelector(".navbar-menu");
+  const navbar_menu_list = document.querySelectorAll(".navbar-menu a");
+  const logo = document.querySelector(".logo");
+  const body = document.body;
+
+  // when toggle click
+  toggle.addEventListener("click", function () {
+    toggle_body.classList.toggle("dark-mode");
+    toggle.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
+    navbar_menu.classList.toggle("dark-mode");
+    navbar.classList.toggle("dark-mode");
+    logo.classList.toggle("dark-mode");
+    navbar_menu_list.forEach((list) => {
+      list.classList.toggle("dark-mode");
+    });
+  });
+}
+
+// Call apps
+navbarbtn();
+darkmode();
